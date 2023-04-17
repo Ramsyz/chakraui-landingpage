7c3961f42233cf6cb1bad6beda4b16a0c847473f
Ramsyz <seetaram.vannemreddigmail.com>
2023-04-17 11:23:37 -0400

    commiting code

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+'use client';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<ChakraProvider>
+				<Navbar />
+				{children}
+				<Footer />
+			</ChakraProvider>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Features from '@/components/Features';
+import Hero from '@/components/Hero';
+
+export default function Home() {
+	return (
+		<>
+			<Hero />
+			<Features />
+		</>
+	);
+}
